f19b0388ab3f127092cea9483379fee8e3dfa4bc
Pascal Hartig <phartig@rdrei.net>
2021-12-13 12:05:54 +0000

    API parsin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
 use std::collections::HashMap;
 
+#[derive(Debug, serde::Deserialize)]
+struct Config {
+    postcode: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+enum Intensity {
+    Low,
+    Moderate,
+    High,
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct RegionalResponse {
+    forecast: u64,
+    index: Intensity,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
+    pretty_env_logger::init();
+    let config = Config {
+        postcode: "E1W".to_string(),
+    };
+    let url = format!(
+        "https://api.carbonintensity.org.uk/regional/postcode/{}",
+        &config.postcode
+    );
+    log::trace!("{}", url);
+    let resp = reqwest::get(&url).await?.text().await?;
+    log::info!("{:#?}", resp);
     Ok(())
 }
